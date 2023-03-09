420f1c6f5d14bdbace273c593dfd2cc14289317b
lukasztymczak <lukasz.tymczak@essentialskillz.com>
2023-03-09 15:58:48 +0000

    ping with exit status conditional statement

--- Section3/ping(exitStatus).sh
+++ Section3/ping(exitStatus).sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-HOST="gocogle.com"
+HOST="google.com"
 
 ping -c 1 $HOST
-if [ "$?" -eq "0" ]
+if [ "$?" -eq "0" ] #'-eq' = 'equal' and '-ne' = 'non equal'
 then
     echo "$HOST reachable"
 else
